132ff49b2f1d0ab69254eda52fe265e54ab98624
yonseung kim <yonseung.kim@gmail.com>
2017-08-29 14:42:22 -0400

    update imports.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { SharedModule } from './shared/shared.module';
-import { AppRoutingModule } from './../app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,4 +1,4 @@
-import { AppRoutingModule } from './../../app-routing.module';
+import { AppRoutingModule } from '../app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { NgModule } from '@angular/core';
